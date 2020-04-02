573da3720e688e797428f82c55dda52c12f5325e
connectedteamCentria <connectedteamCentria@gmail.com>
2020-04-02 20:11:21 +0300

    basic project

---
+++ Pets/src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine($"Hello World!");
+        }
+    }
+}
