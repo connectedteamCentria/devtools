c377b1ee9edcdbb9f78f40675c08a5968f677edf
Sammien <sammi.sjodahl@passagen.se>
2020-05-03 09:42:17 +0300

    testingDoggie

---
+++ Pets/test/UnitTestDog.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+
+namespace Pets
+{
+    public class DogTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+
+        public void DogTalkToOwnerReturnsMeow()
+        {
+            Assert.Pass();
+        }
+
+            
+    }
+}
